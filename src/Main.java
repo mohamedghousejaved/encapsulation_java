afc03cb2b6df89b1ca1de13209ef9e08b1c3cb6f
MohamadGhouseJaved <mohamedghousejaved@gmail.com>
2023-09-04 18:02:05 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        newclass myObj = new newclass();
+        myObj.setName("John");
+        System.out.println(myObj.getName());
+    }
+}
